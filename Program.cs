7dea1826418f6b3ba7d1f5b118d6ab6b4441fe90
Hamza Khan <114700250+hamzaKhan05@users.noreply.github.com>
2024-07-11 01:13:52 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -16,6 +16,13 @@ namespace Assignment2
             //LargestAndSecondLargest();
             //FirstNonRepeating();
             //MergeTwoSortedArrays();
+            //MissingNumberInarray();
+            //ArmstrongNumber();
+            //LongestCommonPrefix();
+            //FibonacciSeries();
+            PositiveAndNegativeCount();
+
+
             Console.WriteLine("Press Enter to exit.");
             Console.ReadLine();
 
@@ -140,8 +147,8 @@ namespace Assignment2
 
         static void MergeTwoSortedArrays()
         {
-            int[] Array1 = {1, 3, 5, 7};
-            int[] Array2 = {2, 4, 6, 8};
+            int[] Array1 = { 1, 3, 5, 7 };
+            int[] Array2 = { 2, 4, 6, 8 };
             int[] SortedMergedArray = new int[Array1.Length + Array2.Length];
             int IndexArray1 = 0, IndexArray2 = 0, IndexNewArray = 0; //iterators for each array initialized with zero to start with first index
 
@@ -168,7 +175,7 @@ namespace Assignment2
                 IndexArray1++;
             }
 
-            while (IndexArray2< Array2.Length) // when length of Array1 is smaller than Array2 and Array2 still has elements in it
+            while (IndexArray2 < Array2.Length) // when length of Array1 is smaller than Array2 and Array2 still has elements in it
             {
                 SortedMergedArray[IndexNewArray] = Array2[IndexArray2];
                 IndexNewArray++;
@@ -177,10 +184,129 @@ namespace Assignment2
             Console.WriteLine("Merged Array: " + string.Join(", ", SortedMergedArray));
         }
 
+        static void LongestCommonPrefix()
+        {
+            string[] ArrayToCheck = {"internet", "intermediate", "interstellar" };
 
+            if (ArrayToCheck == null || ArrayToCheck.Length == 0) //for edge cases
+                Console.WriteLine("");
 
+            string prefix = ArrayToCheck[0]; //we are storing the first string to check it with other strings
 
+            for (int i = 1; i < ArrayToCheck.Length; i++)
+            {              
+                //indexof returns the startind index 0 if both starts with same prefix otherwise -1, as here its internet, intermediate does not starts with internet so its -1
+                while (ArrayToCheck[i].IndexOf(prefix) != 0) // While the current string does not start with the current prefix, trim the prefix from end
+                {
+                    prefix = prefix.Substring(0, prefix.Length - 1);
+                    if (prefix.Length == 0)
+                        Console.WriteLine("");
+                }
+            }
+            Console.Write("The Longest Common Prefix in array [{0}]", string.Join(", ", ArrayToCheck));
+            Console.WriteLine($" is: {prefix}");
+        }
 
+        static void MissingNumberInarray()
+        {
+            //we will use sum technique here, as we have formula for calculating the sum of numbers from 0.
+            int[] ArrayToCheck = { 0, 2, 4, 7, 6, 1, 3 }; // 5 is missing
+            int MissingNumber;
+            int n = ArrayToCheck.Length; // to calculate the sum of all the numbers in the array including missing no. as 0 is counted as 1
+            int SumOfArrayIncludingMissing = n * (n + 1) / 2; // Sum of first all numbers
+
+            int SumOfArray = 0;
+            foreach (int number in ArrayToCheck)
+            {
+                SumOfArray = SumOfArray + number;
+            }
+
+            MissingNumber = SumOfArrayIncludingMissing - SumOfArray;
+            Console.Write("Missing Number in [{0}]", string.Join(", ", ArrayToCheck));
+            Console.WriteLine($" is: {MissingNumber}");
+
+        }
+
+        static void ArmstrongNumber()
+        {
+            int number = 153, LastDigit, SumToCheck = 0;
+            int OriginalNumber = number;
+            while (number != 0)
+            {
+                LastDigit = number % 10;
+                int LengthOfNumber = OriginalNumber.ToString().Length;
+                SumToCheck = SumToCheck + (int)Math.Pow(LastDigit, LengthOfNumber);
+                number = number / 10;
+            }
+            if (SumToCheck == OriginalNumber)
+            {
+                Console.WriteLine($"{OriginalNumber} is an Armstrong number!");
+            }
+            else
+            {
+                Console.WriteLine($"{OriginalNumber} is not an Armstrong number!");
+            }
+
+        }
+        
+        static void FibonacciSeries()
+        {
+            Console.Write("Enter the n for Fibonacci sequence: ");
+            int NumOfTerms = int.Parse(Console.ReadLine());
+            int FirstNumber = 0, SecondNumber = 1, NextNumber;
+
+            Console.WriteLine($"Fibonacci sequence up to {NumOfTerms} terms:");
+
+            for (int i = 0; i < NumOfTerms; i++)
+            {
+                if (i <= 1)
+                {
+                    NextNumber = i;
+                }
+                else
+                {
+                    NextNumber = FirstNumber + SecondNumber;
+                    FirstNumber = SecondNumber;
+                    SecondNumber = NextNumber;
+                }
+                Console.Write(NextNumber + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        static void PositiveAndNegativeCount()
+        {
+            Console.Write("Enter no of integers you wanna check: ");
+            int TotalCount = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[TotalCount];
+            int PosNumberCount = 0, NegNumberCount = 0, Sum = 0;
+
+            Console.Write($"Enter {TotalCount} values: ");
+            string[] inputValues = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < TotalCount; i++)
+            {
+                numbers[i] = int.Parse(inputValues[i]);
+                if (numbers[i] > 0)
+                {
+                    PosNumberCount++;
+                }
+                else if (numbers[i] < 0)
+                {
+                    NegNumberCount++;
+                }
+                Sum += numbers[i];
+            }
+
+            double average = (double)Sum / TotalCount;
+
+            Console.WriteLine($"The no. of positive numbers: {PosNumberCount}");
+            Console.WriteLine($"The no. of negative numbers: {NegNumberCount}");
+            Console.WriteLine($"Sum: {Sum}");
+            Console.WriteLine($"Avg of all numbers: {average}");
+        }
 
     }
 }
